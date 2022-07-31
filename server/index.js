2d28a0a050f05cd3b48cafab73f00800bf039d22
Denis Elezi <deniselezi2003@outlook.com>
2022-07-31 22:26:08 +0100

    Added cors, added reading from data and sending to client

--- server/index.js
+++ server/index.js
@@ -1,1 +1,27 @@
+const fs = require("fs/promises");
 const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = 3001;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('this-or-that backend')
+})
+
+app.get('/questions/:id', function(req, res) {
+  const dataFile = "./data.json";
+  const id = req.params.id;
+  fs.readFile(dataFile, 'utf-8').then(data => {
+    arr = JSON.parse(data);
+    res.send(arr[id - 1]);
+  }).catch(err => {
+    console.log(err);
+  })
+});
+
+app.listen(port, function() {
+  console.log(`Server listening on port ${port}`);
+});
