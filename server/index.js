6b0c8ffccfb6cc14181dfa8fab99420326e90b44
Denis Elezi <deniselezi2003@outlook.com>
2022-08-03 07:17:37 +0100

    Backend now updates json data file

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3001;
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('this-or-that backend')
@@ -20,6 +21,30 @@ app.get('/questions/:id', function(req, res) {
   }).catch(err => {
     console.log(err);
   })
+});
+
+app.post('/post/:id', function(req, res) {
+  const dataFile = "./data.json";
+  const id = req.params.id - 1;
+  fs.readFile(dataFile, 'utf-8')
+    .then(data => {
+      const obj = JSON.parse(data);
+      obj[id] = req.body;
+      return obj;
+    })
+    // UNCOMMENT THIS .THEN TO ACTUALLY UPDATE THE JSON
+    // .then(obj => {
+    //   fs.writeFile(dataFile, JSON.stringify(obj), err => {
+    //     if (err) {
+    //       console.error(err);
+    //       res.send("Error updating JSON.");
+    //     }
+    //   })
+    // })
+    .catch(err => {
+      console.error(err);
+      res.send(err);
+    })
 });
 
 app.listen(port, function() {
